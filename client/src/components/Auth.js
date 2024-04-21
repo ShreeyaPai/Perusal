34d8d2010662131db8380957f6da866a96a3d35c
Kiran Bharadwaj H D <hdkiranbharadwaj@gmail.com>
2024-04-21 23:07:06 +0530

    Touch of De Legend

---
+++ client/src/components/Auth.js
@@ -0,0 +1,45 @@
+import { Fragment, useEffect, useState } from "react";
+import Login from "./Authentication/Login";
+import Signup from "./Authentication/Signup";
+import Home from "./Home";
+
+function Auth(params) {
+  const [userPresent, setUserPresent] = useState(false);
+  const [signupData, setSignUpData] = useState();
+  const [atHome, setAtHome] = useState(true);
+  function HandleSetSignUpData(val) {
+    setSignUpData(val);
+  }
+  function HandleSetUserPresent(val) {
+    setUserPresent(val);
+  }
+  function HandleSetAtHome(val) {
+    setAtHome(val);
+  }
+  useEffect(() => {
+    HandleSetAtHome(false);
+    setUserPresent(true);
+  }, []);
+  if (atHome) {
+    return (
+      <Home PAtHome={HandleSetAtHome} PUserPresent={HandleSetUserPresent} />
+    );
+  } else if (!atHome && !userPresent) {
+    return (
+      <Fragment>
+        <Signup
+          PUserPresent={HandleSetUserPresent}
+          PHSignUpData={HandleSetSignUpData}
+        />
+      </Fragment>
+    );
+  } else if (!atHome && userPresent) {
+    return (
+      <Fragment>
+        <Login PUserPresent={HandleSetUserPresent} />
+      </Fragment>
+    );
+  }
+}
+
+export default Auth;
